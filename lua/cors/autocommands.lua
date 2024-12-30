375bd2e1fb6bf3f386793b97dd390b8e07fe1bcb
douglas <douglasmaxton@gmail.com>
2024-12-30 17:21:32 +0000

    feat(added): closes all hidden buf on buf leave

--- lua/cors/autocommands.lua
+++ lua/cors/autocommands.lua
@@ -27,10 +27,44 @@ vim.api.nvim_create_autocmd("FileType", {
 -- Save fold on buffer leave
 api.nvim_create_autocmd({ "BufWinLeave" }, {
 	pattern = "*",
-	command = "mkview",
+    callback = function()
+        -- Get the current buffer's filetype and name
+        local filetype = vim.bo.filetype
+        local bufname = api.nvim_buf_get_name(0)
+
+        -- skip buffers with no name or specific filetypes like TelescopePrompt 
+        if filetype == "TelescopePrompt" or filetype == "nofile" or bufname == "" then
+            return
+        end
+
+        vim.cmd("silent! mkview")
+    end,
 })
 -- Restore fold on buffer enter
 api.nvim_create_autocmd({ "BufWinEnter" }, {
 	pattern = "*",
 	command = "silent! loadview",
+})
+
+-- Close all hidden buffers on BufWinLeave 
+api.nvim_create_autocmd({ "BufWinLeave" }, {
+    pattern = "*",
+    callback = function()
+        -- Get the current buffer's filetype
+        local filetype = vim.bo.filetype
+
+        -- Skip buffers with filetype TelescopePrompt or no name
+        if filetype == "TelescopePrompt" or api.nvim_buf_get_name(0) == "" then
+            return
+        end
+
+        -- Iterate over all buffers
+        for _, buf in ipairs(api.nvim_list_bufs()) do
+            -- check if the buffer is loaded and hidden
+            if api.nvim_buf_is_loaded(buf) and vim.bo[buf].buflisted and #vim.fn.win_findbuf(buf) == 0 then
+                -- Deletes the buffer
+                api.nvim_buf_delete(buf, { force = true })
+            end
+        end
+    end,
 })
