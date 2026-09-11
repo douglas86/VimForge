375d90de6ab442a6ef1257456cb729651c0b1413
douglas <douglasmaxton@gmail.com>
2026-09-11 20:43:37 +0100

    refectored: markdown plugins to its own file

--- lua/plugins/main.lua
+++ lua/plugins/main.lua
@@ -65,26 +65,7 @@ return {
             automatic_enable = true,
         },
     },
-    {
-        "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        ft = { "markdown" },
-        build = function() vim.fn["mkdp#util#install"]() end,
-    },
-    {
-    "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-    ft = { "markdown" },
-    opts = {
-      heading = {
-        enabled = true,
-        sign = false,
-        position = "inline",
-        width = "full",
-        icons = { " ", " ", " ", " ", " ", " "},
-      },
-    },
-  },{
+  {
         "folke/lazydev.nvim",
         ft = 'lua', -- only load on lua files
         opts = {
---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,24 @@
+-- markdown.lua
+
+return {
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end,
+    },
+    {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+    ft = { "markdown" },
+    opts = {
+      heading = {
+        enabled = true,
+        sign = false,
+        position = "inline",
+        width = "full",
+        icons = { " ", " ", " ", " ", " ", " "},
+      },
+    },
+}
+}
